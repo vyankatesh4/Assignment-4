4dc439646d98613ba5fb290200887fc27d67bc32
vyankatesh dube <93048498+vyankatesh4@users.noreply.github.com>
2021-12-04 12:35:30 +0530

    Add files via upload

---
+++ A4 Q2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+#Write a Python program to triple all numbers of a given list of integers. Use Python map.
+
+
+
+#sample list: [1, 2, 3, 4, 5, 6, 7]
+
+def fun1(a):
+    return (a*3)
+print('Triple of list numbers:')
+print(list(map(fun1,[1, 2, 3, 4, 5, 6, 7])))
+
+
+# In[ ]:
+
+
+
+
