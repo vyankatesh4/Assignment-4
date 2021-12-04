cf6894fa271be17ae7bd44c4e301affc592c99b2
vyankatesh dube <93048498+vyankatesh4@users.noreply.github.com>
2021-12-04 12:20:29 +0530

    Add files via upload

---
+++ A4 Q1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+
+y = lambda a: a+25
+print(y(10))
+
+
+# In[ ]:
+
+
+
+
